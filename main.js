610d4d2034df099b68aa463bfa5391113f5947d6
Islombek Mirsalimov <mirsalimovislombek9@gmail.com>
2026-01-13 12:06:43 +0500

    add a ready hash method

---
+++ main.js
@@ -0,0 +1,19 @@
+class Hashmap {
+    constructor() {
+        let loadFactor = 0.75;
+        let capacity = 16;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+        return hashCode;
+    }
+}
+
+
+const hashMap = new Hashmap();
+console.log(hashMap.hash("yek"));
